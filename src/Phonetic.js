732062fd2d1ccad3bf811097d8dc0bf94d2965db
lamoncso <laukomonika@gmail.com>
2023-10-04 19:29:47 +0200

    Add audio icon

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,7 @@ export default function Phonetic(props){
     return (
       <div className="Phonetic">
         <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-          Listen
+          <i class="fa-solid fa-volume-high"></i>
         </a>
         <br />
         {props.phonetic.text}
