9131cbc320a5b13e107d5650715122e23b396aba
lamoncso <laukomonika@gmail.com>
2023-10-04 19:23:34 +0200

    Change link in Phonetics component

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props){
-    return(
-        <div className="Phonetic">
-           <a href={props.phonetic.audio} target="_blank">
-            Listen
-           </a>
-           <br/>
-            {props.phonetic.text}
-        </div>
-    )
+    return (
+      <div className="Phonetic">
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+          Listen
+        </a>
+        <br />
+        {props.phonetic.text}
+      </div>
+    );
 }
