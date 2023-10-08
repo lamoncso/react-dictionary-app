77700ee187dc29173b558ceeaece6702f3c5b907
lamoncso <laukomonika@gmail.com>
2023-10-08 16:06:41 +0200

    Add alt to img and remove it from the link

--- src/Photos.js
+++ src/Photos.js
@@ -2,20 +2,22 @@ import React from "react";
 import "./Photos.css";
 
 export default function Photos(props){
-    console.log(props.photos)
     if (props.photos) {
         return(
             <section className="Photos">
                 <div className="row">
                 {props.photos.map(function(photo, index){
-                    return(
-                        <div key={index} className="col-4 img-fluid">
-                            <a href={photo.src.original} target="_blank" rel="noreferrer"
-                            alt="">
-                        <img src={photo.src.tiny}/>
+                    return (
+                      <div key={index} className="col-4 img-fluid">
+                        <a
+                          href={photo.src.original}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          <img src={photo.src.tiny} alt="" />
                         </a>
-                        </div>
-                    )
+                      </div>
+                    );
                 }
                 )}
                 </div>
